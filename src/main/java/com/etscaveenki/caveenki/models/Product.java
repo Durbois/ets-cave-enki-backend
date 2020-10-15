945df5bd0c524ff051473fa7ed78628c2326ea13
tfongue <hadrien.fongue@prodyna.com>
2020-10-15 12:40:14 +0200

    add product entity attributes

--- src/main/java/com/etscaveenki/caveenki/models/Product.java
+++ src/main/java/com/etscaveenki/caveenki/models/Product.java
@@ -1,8 +1,12 @@
 package com.etscaveenki.caveenki.models;
 
+import com.etscaveenki.caveenki.models.enums.ContentType;
+import com.etscaveenki.caveenki.models.enums.ProductType;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
 @Table(name = "products")
@@ -16,6 +20,86 @@ public class Product {
     @Size(max = 50)
     private String name;
 
-    private String contentShape;
-    private String type;
+    @Enumerated(EnumType.STRING)
+    private ContentType contentType;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType productType;
+
+    @NotBlank
+    private double unitPrice;
+
+    @NotBlank
+    private double boxPrice;
+
+    private Set <String> ingredients;
+
+    public Product() {
+    }
+
+    public Product(String name, ContentType contentType, ProductType productType,
+                   double unitPrice, double boxPrice, Set<String> ingredients) {
+        this.name = name;
+        this.contentType = contentType;
+        this.productType = productType;
+        this.unitPrice = unitPrice;
+        this.boxPrice = boxPrice;
+        this.ingredients = ingredients;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(ContentType contentType) {
+        this.contentType = contentType;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public void setProductType(ProductType productType) {
+        this.productType = productType;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getBoxPrice() {
+        return boxPrice;
+    }
+
+    public void setBoxPrice(double boxPrice) {
+        this.boxPrice = boxPrice;
+    }
+
+    public Set<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<String> ingredients) {
+        this.ingredients = ingredients;
+    }
 }
---
+++ src/main/java/com/etscaveenki/caveenki/models/enums/ProductType.java
@@ -0,0 +1,8 @@
+package com.etscaveenki.caveenki.models.enums;
+
+public enum ProductType {
+
+    WHITE_WINE,
+    RED_WHINE,
+    WHISKY
+}
