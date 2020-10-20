8159c3be240a58054e9bc257c2694d437115d193
tfongue <hadrien.fongue@prodyna.com>
2020-10-20 16:23:42 +0200

    Controller junit test with template

--- src/main/java/com/etscaveenki/caveenki/controllers/ProductController.java
+++ src/main/java/com/etscaveenki/caveenki/controllers/ProductController.java
@@ -2,8 +2,11 @@ package com.etscaveenki.caveenki.controllers;
 
 import com.etscaveenki.caveenki.models.Product;
 import com.etscaveenki.caveenki.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,24 +16,26 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping(value = "/api/product")
 public class ProductController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Value("${caveenki.sorted.type}")
-    private String sortedType;
+    private String sortedType = "productType";
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/all")
-    public ResponseEntity<?> findAllProductsSortedBy(@RequestParam(required = false) String sortedby) {
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> findAllProductsSortedBy(@RequestParam(required = false) String sortedBy) {
 
         List<Product> products;
 
-        if (sortedby == null) {
+        if (sortedBy == null) {
             products = productService.getAllProductsSortBy(sortedType);
         } else {
-            products = productService.getAllProductsSortBy(sortedby);
+            products = productService.getAllProductsSortBy(sortedBy);
         }
 
         return ResponseEntity.ok().body(products);
--- src/test/java/com/etscaveenki/caveenki/controllers/ProductControllerTest.java
+++ src/test/java/com/etscaveenki/caveenki/controllers/ProductControllerTest.java
@@ -3,89 +3,71 @@ package com.etscaveenki.caveenki.controllers;
 import com.etscaveenki.caveenki.models.Product;
 import com.etscaveenki.caveenki.models.enums.ContentType;
 import com.etscaveenki.caveenki.models.enums.ProductType;
-import com.etscaveenki.caveenki.security.services.UserDetailsServiceImpl;
 import com.etscaveenki.caveenki.services.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @RunWith(SpringRunner.class)
-// @WebMvcTest(ProductController.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductControllerTest {
 
-//    @Autowired
-//    private WebApplicationContext context;
-
     @Autowired
-    private TestRestTemplate template;
-
-//    @Autowired
-//    private MockMvc mvc;
+    private ObjectMapper objectMapper;
 
     @MockBean
     private ProductService productService;
 
-    /*@Before
-    public void setup() {
-        mvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-    }*/
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Before
+    public void setUp() {
+       // ReflectionTestUtils.setField(ProductController.class, "sortedType", "productType");
+    }
 
     @Test
     public void shoudListAllProducts() throws Exception {
 
         when(productService.getAllProductsSortBy(anyString())).thenReturn(Collections.singletonList(getProduct()));
 
-//        mvc.perform(get("/api/product/all")
-//        .contentType(MediaType.APPLICATION_JSON))
-//        .andExpect(status().isOk())
-//        .andExpect(jsonPath("$", hasSize(1)));
+        ResponseEntity<?> responseEntity = restTemplate.getForEntity("/api/product/all", List.class);
 
-        ResponseEntity<?> result = template.getForEntity("/api/product/all", List.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        assert(HttpStatus.OK == result.getStatusCode());
+        assertThat(responseEntity.getBody().equals(Collections.singletonList(getProduct())));
     }
 
     @Test
-    @WithMockUser(roles = {"ADMIN"})
+    // @WithMockUser(roles = {"ADMIN"})
     public void shoudCreateProduct(){
 
+        when(productService.createNewProduct(any(Product.class))).thenReturn(getProduct());
+
+        ResponseEntity<?> responseEntity = restTemplate.postForEntity("/api/product", getProduct(), Product.class);
+
+        assertThat(responseEntity.getBody().equals(getProduct()));
     }
 
     private Product getProduct() {
-        return new Product("name", ContentType.BOTTLE, ProductType.RED_WINE,
+        return new Product("product", ContentType.BOTTLE, ProductType.RED_WINE,
                 10.3, 1000, "Banana;Aroma");
     }
 
